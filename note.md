396ab62d793b4c5ac9e4d9cdcf229b98a9756d49
sca-developers <developers@sca.com.au>
2018-03-02 14:33:24 +1100

    init

--- note.md
+++ note.md
@@ -2,3 +2,7 @@
 yarn add react react-dom react-prop-types react-router-dom semantic-ui-react
 
 yarn add babel-core babel-loader babel-preset-env babel-preset-react babel-preset-stage-1 css-loader style-loader webpack-contrib/html-webpack-plugin webpack webpack-dev-server webpack-cli -D
+
+yarn add react-hot-loader -D
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,54 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 const port = process.env.PORT || 3000
 
 module.exports = {
-    mode: 'development'
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            //js rule
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: ['babel-loader']
+            },
+
+            //styles
+            {
+              test: /\.css$/,
+              use: [
+                {
+                  loader: 'style-loader'
+                },
+                {
+                  loader: 'css-loader',
+                  options: {
+                    modules: true, //loader use css modules to make css working in js,
+                    localIdentName: '[folder]-[local]-[hash:12]',
+                    camelCase: true,
+                    sourceMap: true
+                  }
+                }
+              ]
+            }
+        ]
+    },
+    plugins: [
+      new htmlWebpackPlugin({
+        template: 'public/index.html',
+        favicon: 'public/favicon.ico'
+      })
+    ],
+    devServer: {
+      host: 'localhost',
+      port: port,
+      historyApiFallback: true,
+      open: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
 }
